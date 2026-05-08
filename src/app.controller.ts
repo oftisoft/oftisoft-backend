e9f1b773d8fb484843bab955d20d6966ac0e3a3f
Rasel Hossain <rahatulislam67777@gmail.com>
2026-05-09 05:49:21 +0600

    Add HTML page for root route

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,13 +1,15 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Res } from '@nestjs/common';
 import { AppService } from './app.service';
+import type { Response } from 'express';
+import { join } from 'path';
 
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
   @Get()
-  getHello(): string {
-    return this.appService.getHello();
+  getHello(@Res() res: Response) {
+    res.sendFile(join(process.cwd(), 'public', 'index.html'));
   }
 
   @Get('health')
